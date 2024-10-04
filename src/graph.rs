7e5f2dd4fd07df4e6328a60ef142b262d47a8504
Eric Buehler <ericlbuehler@gmail.com>
2024-10-04 17:02:33 -0400

    Load all ptx files

--- src/graph.rs
+++ src/graph.rs
@@ -1,14 +1,113 @@
 use candle_core::{
+    backend::BackendStorage,
     cuda::cudarc::driver::{
         self,
         sys::{
             CUgraph, CUgraphDebugDot_flags, CUgraphExec, CUgraphInstantiate_flags, CUstream,
             CUstreamCaptureMode,
         },
+        DevicePtr, DeviceSlice,
     },
-    DType, Device, Tensor,
+    quantized::{GgmlDType, QMatMul, QTensor},
+    DType, Device, Storage, Tensor,
 };
-use std::{path::Path, process::Command, ptr};
+use candle_nn::Module;
+use half::{bf16, f16};
+use std::{
+    collections::{HashMap, HashSet},
+    path::Path,
+    process::Command,
+    ptr,
+};
+
+/// # Safety
+/// It must be ensured that the storage of src can be cast to &mut. So no aliasing across threads.
+unsafe fn copy_inplace(src: &Tensor, dst: &Tensor, device: &Device) -> anyhow::Result<()> {
+    match (&*src.storage_and_layout().0, &*dst.storage_and_layout().0) {
+        (Storage::Cuda(src), Storage::Cuda(tgt)) => {
+            // What we are really doing:
+
+            // unsafe fn cast_to_mut<T>(r: &T) -> &mut T {
+            //     // Cast immutable reference to mutable reference
+            //     #[allow(invalid_reference_casting)]
+            //     &mut *(r as *const T as *mut T)
+            // }
+            // let dst = unsafe { cast_to_mut(tgt.as_cuda_slice::<bf16>()?) };
+            // cu_device.dtod_copy(src, dst)?;
+
+            anyhow::ensure!(src.dtype() == dst.dtype(), "DTypes must match!");
+
+            match src.dtype() {
+                DType::BF16 => {
+                    let tgt = tgt.as_cuda_slice::<bf16>()?;
+                    let src = src.as_cuda_slice::<bf16>()?;
+                    driver::result::memcpy_dtod_sync(
+                        *tgt.device_ptr(),
+                        *src.device_ptr(),
+                        src.len() * std::mem::size_of::<bf16>(),
+                    )?;
+                }
+                DType::F16 => {
+                    let tgt = tgt.as_cuda_slice::<f16>()?;
+                    let src = src.as_cuda_slice::<f16>()?;
+                    driver::result::memcpy_dtod_sync(
+                        *tgt.device_ptr(),
+                        *src.device_ptr(),
+                        src.len() * std::mem::size_of::<f16>(),
+                    )?;
+                }
+                DType::F32 => {
+                    let tgt = tgt.as_cuda_slice::<f32>()?;
+                    let src = src.as_cuda_slice::<f32>()?;
+                    driver::result::memcpy_dtod_sync(
+                        *tgt.device_ptr(),
+                        *src.device_ptr(),
+                        src.len() * std::mem::size_of::<f32>(),
+                    )?;
+                }
+                DType::F64 => {
+                    let tgt = tgt.as_cuda_slice::<f64>()?;
+                    let src = src.as_cuda_slice::<f64>()?;
+                    driver::result::memcpy_dtod_sync(
+                        *tgt.device_ptr(),
+                        *src.device_ptr(),
+                        src.len() * std::mem::size_of::<f64>(),
+                    )?;
+                }
+                DType::I64 => {
+                    let tgt = tgt.as_cuda_slice::<i64>()?;
+                    let src = src.as_cuda_slice::<i64>()?;
+                    driver::result::memcpy_dtod_sync(
+                        *tgt.device_ptr(),
+                        *src.device_ptr(),
+                        src.len() * std::mem::size_of::<i64>(),
+                    )?;
+                }
+                DType::U32 => {
+                    let tgt = tgt.as_cuda_slice::<u32>()?;
+                    let src = src.as_cuda_slice::<u32>()?;
+                    driver::result::memcpy_dtod_sync(
+                        *tgt.device_ptr(),
+                        *src.device_ptr(),
+                        src.len() * std::mem::size_of::<u32>(),
+                    )?;
+                }
+                DType::U8 => {
+                    let tgt = tgt.as_cuda_slice::<u8>()?;
+                    let src = src.as_cuda_slice::<u8>()?;
+                    driver::result::memcpy_dtod_sync(
+                        *tgt.device_ptr(),
+                        *src.device_ptr(),
+                        src.len() * std::mem::size_of::<u8>(),
+                    )?;
+                }
+            }
+            device.synchronize()?;
+        }
+        _ => unreachable!(),
+    }
+    Ok(())
+}
 
 pub enum GraphDumpFormat {
     Svg,
@@ -26,12 +125,14 @@ pub struct Graph {
     exec: CUgraphExec,
     stream: CUstream,
     device: Device,
+    input_tensors: HashMap<&'static str, Tensor>,
 }
 
 impl Graph {
     pub fn new(
         from_code: impl FnOnce() -> anyhow::Result<()>,
         device: &Device,
+        input_tensors: HashMap<&'static str, Tensor>,
     ) -> anyhow::Result<Self> {
         let cu_device = match &device {
             Device::Cuda(dev) => dev,
@@ -41,16 +142,61 @@ impl Graph {
         let cu_stream = cu_device.cu_stream();
 
         // Initialize all ptx files
+        // `load_ptx` cannot be called while capturing the stream so we need this to happen
+        // beforehand.
         {
-            // load_ptx cannot be called while capturing the stream so we need this to happen
-            // beforehand.
-            let u = Tensor::zeros((4096, 4096), DType::F32, &device)?.to_dtype(DType::BF16)?;
-            let x = Tensor::zeros((4096, 4096), DType::F32, &device)?.to_dtype(DType::BF16)?;
-            let v = Tensor::zeros(4096, DType::F32, &device)?.to_dtype(DType::BF16)?;
-            let _x = x.mul(&u)?.broadcast_add(&v)?;
-            let _x = x.affine(1., 0.5)?;
-            x.slice_set(&u, 0, 0)?;
-            x.matmul(&x)?;
+            // Fill
+            let x = Tensor::zeros((128, 128), DType::F32, device)?;
+
+            // Affine
+            let _ = x.affine(1., 0.5)?;
+
+            // Binary
+            let _ = x.mul(&x)?;
+
+            // Cast
+            let _ = x.to_dtype(DType::BF16)?;
+
+            // Conv2d
+            {
+                let ws = Tensor::zeros((3, 3, 4, 4), DType::F32, device)?;
+                let conv_xs = Tensor::zeros((1, 3, 48, 48), DType::F32, device)?;
+                let _ = conv_xs.conv2d(&ws, 0, 1, 1, 1)?;
+            }
+
+            // Indexing
+            {
+                let indices = Tensor::new(vec![0u32, 2, 4], device)?;
+                let _ = x.index_select(&indices, 0)?;
+            }
+
+            // FUSED_RMS_NORM
+            // TODO
+
+            // FUSED_ROPE
+            // TODO
+
+            // Quantized
+            {
+                let q = QMatMul::from_qtensor(QTensor::quantize(&x, GgmlDType::Q8_0)?)?;
+                let _ = q.forward(&x)?;
+            }
+
+            // Reduce
+            let _ = candle_nn::ops::softmax_last_dim(&x)?;
+
+            // Sort
+            let _ = x.sort_last_dim(true)?;
+
+            // Ternary
+            let _ = x.to_dtype(DType::U32)?.where_cond(
+                &Tensor::new(0f32, device)?.broadcast_as(x.shape())?,
+                &Tensor::new(1f32, device)?.broadcast_as(x.shape())?,
+            )?;
+
+            // Unary
+            let _ = x.neg()?;
+
             device.synchronize()?;
         }
 
@@ -104,11 +250,29 @@ impl Graph {
             exec: cu_graph_e,
             stream: *cu_stream,
             device: device.clone(),
+            input_tensors,
         })
     }
 
     /// Run the graph
-    pub fn replay(&self) -> anyhow::Result<()> {
+    pub fn replay(&self, input_tensors: HashMap<&'static str, &Tensor>) -> anyhow::Result<()> {
+        let mut added = HashSet::new();
+        for (name, input) in &input_tensors {
+            if !added.insert(name) {
+                panic!("Got duplicate inputs {name}");
+            }
+            if let Some(inp_ref) = self.input_tensors.get(name) {
+                unsafe { copy_inplace(input, inp_ref, &self.device)? };
+            } else {
+                panic!("Graph has no input {name}");
+            }
+        }
+        if added.len() != input_tensors.len() {
+            panic!(
+                "Some inputs were not provided: expected {:?}, got {added:?}",
+                input_tensors.keys().collect::<Vec<_>>()
+            );
+        }
         unsafe {
             driver::sys::lib()
                 .cuGraphLaunch(self.exec, self.stream)
--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,66 @@
 //! https://pytorch.org/blog/accelerating-pytorch-with-cuda-graphs/
 //! https://github.com/pytorch/pytorch/blob/c7b0d4b148cf2e4e68f14193549945e1639bff40/aten/src/ATen/cuda/CUDAGraph.cpp
 
-mod graph;
-use graph::{Graph, GraphDumpFormat, GraphDumpVerbosity};
+use cuda_graph::{Graph, GraphDumpFormat, GraphDumpVerbosity};
 
-use std::f64::consts::E;
+use std::{f64::consts::E, time::Instant};
 
-use candle_core::{
-    cuda::cudarc::{
-        self,
-        driver::{DevicePtr, DeviceSlice},
-    },
-    DType, Device, Storage, Tensor,
-};
-use half::bf16;
+use candle_core::{DType, Device, Tensor};
 
-/// # Safety
-/// It must be ensured that the storage of src can be cast to &mut. So no aliasing across threads.
-unsafe fn copy_into(src: &Tensor, dst: &Tensor, device: &Device) -> anyhow::Result<()> {
-    match (&*src.storage_and_layout().0, &*dst.storage_and_layout().0) {
-        (Storage::Cuda(src), Storage::Cuda(tgt)) => {
-            // What we are really doing:
-
-            // unsafe fn cast_to_mut<T>(r: &T) -> &mut T {
-            //     // Cast immutable reference to mutable reference
-            //     #[allow(invalid_reference_casting)]
-            //     &mut *(r as *const T as *mut T)
-            // }
-            // let dst = unsafe { cast_to_mut(tgt.as_cuda_slice::<bf16>()?) };
-            // cu_device.dtod_copy(src, dst)?;
-
-            let tgt = tgt.as_cuda_slice::<bf16>()?;
-            let src = src.as_cuda_slice::<bf16>()?;
-            cudarc::driver::result::memcpy_dtod_sync(
-                *tgt.device_ptr(),
-                *src.device_ptr(),
-                src.len() * std::mem::size_of::<bf16>(),
-            )?;
-            device.synchronize()?;
-        }
-        _ => unreachable!(),
-    }
-    Ok(())
-}
+const N: usize = 100; // 10000;
+const INNER_N: usize = 100;
+const SHAPE: (usize, usize) = (512, 512);
 
 fn main() -> anyhow::Result<()> {
     let device = Device::new_cuda_with_stream(0)?;
 
-    let x = Tensor::ones((4, 4), DType::BF16, &device)?;
+    let x = Tensor::ones(SHAPE, DType::BF16, &device)?;
     let mut y: Option<Tensor> = None;
 
     let graph = Graph::new(
         || {
-            let out_data = x.log()?.matmul(&x)?;
+            let mut out_data = x.matmul(&x)?;
+            for _ in 0..INNER_N {
+                out_data = out_data.matmul(&x)?;
+            }
             y = Some(out_data);
             Ok(())
         },
         &device,
+        [("x", x.clone())].into(),
     )?;
 
     graph.output_dot("out.png", GraphDumpFormat::Png, GraphDumpVerbosity::Verbose)?;
 
-    for i in 1..=10 {
-        println!("{} Exec {i} {}", "=".repeat(10), "=".repeat(10));
-        let new = Tensor::full(E.powi(i), (4, 4), &device)?.to_dtype(DType::BF16)?;
+    let start = Instant::now();
+    for i in 1..=N {
+        let new = Tensor::full(E.powi(i as i32), SHAPE, &device)?.to_dtype(DType::BF16)?;
+        graph.replay([("x", &new)].into())?;
+    }
+    let graph_duration = Instant::now().duration_since(start);
 
-        unsafe { copy_into(&new, &x, &device)? };
-
-        println!("x\n{x}");
-
-        graph.replay()?;
-
-        if let Some(y) = &y {
-            println!("out\n{y}");
+    let start = Instant::now();
+    for i in 1..=N {
+        let x = Tensor::full(E.powi(i as i32), SHAPE, &device)?.to_dtype(DType::BF16)?;
+        let mut out_data = x.matmul(&x)?;
+        for _ in 0..INNER_N {
+            out_data = out_data.matmul(&x)?;
         }
     }
+    let eager_duration = Instant::now().duration_since(start);
+    println!(
+        "Graph run took {}s",
+        graph_duration.div_f32(N as f32).as_secs_f32()
+    );
+    println!(
+        "Eager run took {}s",
+        eager_duration.div_f32(N as f32).as_secs_f32()
+    );
+    println!(
+        "Graph is {} faster",
+        eager_duration.div_f32(N as f32).as_secs_f32()
+            / graph_duration.div_f32(N as f32).as_secs_f32()
+    );
 
     Ok(())
 }
